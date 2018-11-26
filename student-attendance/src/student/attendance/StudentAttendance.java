c0cf459994d896667f7049bead26e42ce3cbea5a
Anthony Mcgarry <anthony.mcgarry@wearenova.co.uk>
2018-11-26 15:46:48 +0000

    Added studentdb make changes to the database to make the fields required from student and tutor class

--- student-attendance/src/student/attendance/StudentAttendance.java
+++ student-attendance/src/student/attendance/StudentAttendance.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package student.attendance;
-
+import java.sql.*;
 import static java.time.Clock.system;
 
 /**
@@ -18,7 +18,21 @@ public class StudentAttendance {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+           Class.forName("org.apache.derby.jdbc.EmbeddedDriver"); 
+        }catch(ClassNotFoundException e){
+            System.err.println(e);
+        }
         
+        try{
+            Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/cis4005", "cis4005", "cis4005");
+            Statement stmt = con.createStatement();
+           
+            ResultSet st = stmt.executeQuery("SELECT * from student");
+
+        }catch(SQLException e){
+            System.err.println(e);
+        }
     }
     
 }
