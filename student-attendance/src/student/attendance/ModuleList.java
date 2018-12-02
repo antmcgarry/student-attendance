e02b9a6be74aa0f85e2a5d980baaaa4f7350a0d0
Anthony Mcgarry <anthony.mcgarry@wearenova.co.uk>
2018-12-02 19:37:15 +0000

    Created a ModuleList manager class

---
+++ student-attendance/src/student/attendance/ModuleList.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student.attendance;
+
+import java.util.ArrayList;
+import model.Module;
+
+/**
+ *
+ * @author Anthony Mcgarry
+ */
+public class ModuleList {
+        private final ArrayList<Module> moduleList;
+    
+    public ModuleList(){
+        moduleList = new ArrayList<>();
+    }
+    
+    public void add(Module m){
+        moduleList.add(m);
+    }
+    
+    public Module get(int i){
+        return moduleList.get(i);
+    }
+    
+    public void remove(Module m){
+        moduleList.remove(m);
+    }
+    
+    public ArrayList<Module> getProgrammes() {
+        return moduleList;
+    }
+    
+    public int Size(){
+        return moduleList.size();
+    }
+}
